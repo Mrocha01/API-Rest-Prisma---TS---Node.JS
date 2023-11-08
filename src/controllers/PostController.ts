52b1a3a591fa6112e10940ae8ff7f45dbb80ce05
MateusR <mateusftc_@hotmail.com>
2023-11-07 22:26:15 -0300

    feat: Adicionado o get allPosts
    Fiz questão de formatar a data para o formato BR

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../db";
+import { format } from "date-fns";
 
 export default {
   async createPost(req: Request, res: Response) {
@@ -38,6 +39,33 @@ export default {
         error: false,
         message: "Post criado com sucesso!",
         post,
+      });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro interno do servidor." });
+    }
+  },
+
+  async allPosts(req: Request, res: Response) {
+    try {
+      const data = await prisma.post.findMany();
+
+      if (!data) {
+        return res
+          .status(404)
+          .json({ error: true, message: "Posts não encontrados!" });
+      }
+
+      const formattedData = data.map((post) => {
+        return {
+          ...post,
+          published: format(post.published, "dd/MM/yyyy HH:mm:ss"),
+        };
+      });
+
+      return res.status(201).json({
+        error: false,
+        message: "Posts encontrados com sucesso!",
+        formattedData,
       });
     } catch (error) {
       return res.status(500).json({ message: "Erro interno do servidor." });
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -4,5 +4,6 @@ import PostController from "../controllers/PostController";
 const router = express.Router();
 
 router.post("/create", PostController.createPost);
+router.get("/all", PostController.allPosts);
 
 export default router;
